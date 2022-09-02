1eb401cba3ecb4d7e044e12dcb2164d79cd5ffcc
Victor <victorhenzzowp@gmail.com>
2022-09-02 15:57:31 -0300

    Adding get_it

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,20 @@
+import 'package:bmi_calculator/modules/bmi_calculator/data/repository/get_bmi_result_repository.dart';
+import 'package:bmi_calculator/modules/bmi_calculator/ports/input/get_bmi_result_input_port.dart';
+import 'package:bmi_calculator/modules/bmi_calculator/ports/output/get_bmi_result_output_port.dart';
 import 'package:bmi_calculator/modules/bmi_calculator/routes/bmi_calculator_route.dart';
+import 'package:bmi_calculator/modules/bmi_calculator/usecase/get_bmi_result_use_case.dart';
 import 'package:bmi_calculator/navigation/app_navigation.dart';
 import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
 
 void main() {
+  GetIt getIt = GetIt.I;
+
+  getIt.registerSingleton<GetBmiResultOutputPort>(GetBmiResultRepository());
+  getIt.registerSingleton<GetBmiResultInputPort>(GetBmiResultUseCase(
+    getIt<GetBmiResultOutputPort>(),
+  ));
+
   runApp(const MyApp());
 }
 
